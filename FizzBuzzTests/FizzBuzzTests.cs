0b1a511bd56592c2647b2c7aa721a0287236067c
Jacob Rutter <rutter.jacob@gmail.com>
2019-05-22 19:24:10 -0400

    made FizzBuzzTests a little more dry
    Removed the instantiation of FizzBuzz object for each test. Doing it once before each test now. I don't think there is a need to make use of dispose in this use case.

--- FizzBuzzTests/FizzBuzzTests.cs
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -4,24 +4,24 @@ namespace FizzBuzzKata
 {
   public class FizzBuzzTests
   {
+
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
     [Fact]
     public void whenFizzBuzzIsPassedANumberItReturnsThatNumber()
     {
-      FizzBuzz fizzBuzz = new FizzBuzz();
       Assert.Equal("1", fizzBuzz.Fizz(1));
     }
 
     [Fact]
     public void whenFizzBuzzIsPassedTwoItReturnsTwo()
     {
-      FizzBuzz fizzBuzz = new FizzBuzz();
       Assert.Equal("2", fizzBuzz.Fizz(2));
     }
 
     [Fact]
     public void whenFizzBuzzIsPassedAMultipleOfThreeItReturnsFizz()
     {
-      FizzBuzz fizzBuzz = new FizzBuzz();
       Assert.Equal("fizz", fizzBuzz.Fizz(3));
       Assert.Equal("fizz", fizzBuzz.Fizz(6));
     }
@@ -29,7 +29,6 @@ namespace FizzBuzzKata
     [Fact]
     public void whenFizzBuzzIsPassedAMultipleOfFiveItReturnsBuzz()
     {
-      FizzBuzz fizzBuzz = new FizzBuzz();
       Assert.Equal("buzz", fizzBuzz.Fizz(5));
       Assert.Equal("buzz", fizzBuzz.Fizz(10));
     }
@@ -37,7 +36,6 @@ namespace FizzBuzzKata
     [Fact]
     public void whenFizzBuzzIsPassedAMultipleOfThreeAndFiveItReturnsFizzBuzz()
     {
-      FizzBuzz fizzBuzz = new FizzBuzz();
       Assert.Equal("fizzBuzz", fizzBuzz.Fizz(15));
       Assert.Equal("fizzBuzz", fizzBuzz.Fizz(45));
     }
